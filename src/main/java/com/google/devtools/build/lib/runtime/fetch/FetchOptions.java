904bee288d691247280b2c31e25bad1577151ac8
Ulf Adams <ulfjack@google.com>
2016-12-04 09:47:38 -0500

    Move fetch commmand to runtime.fetch.

--- src/main/java/com/google/devtools/build/lib/runtime/fetch/FetchOptions.java
+++ src/main/java/com/google/devtools/build/lib/runtime/fetch/FetchOptions.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.devtools.build.lib.bazel.commands;
+package com.google.devtools.build.lib.runtime.fetch;
 
 import com.google.devtools.common.options.Option;
 import com.google.devtools.common.options.OptionsBase;
